df11330f91489d0693a9c8c0e70327bcd8254de8
Al Naheyan <officialk2000@gmail.com>
2024-10-08 21:48:45 -0400

    cxx change

--- sequence3.cxx
+++ sequence3.cxx
@@ -6,10 +6,10 @@
 namespace main_savich_5 {
     
     sequence::seqeuence() {
-        head_ptr = nullptr; 
-        tail_ptr = nullptr;
-        cursor = nullptr;
-        precursor = nullptr;
+        head_ptr = NULL; 
+        tail_ptr = NULL;
+        cursor = NULL;
+        precursor = NULL;
         used = 0;
     }
 
@@ -17,11 +17,15 @@ namespace main_savich_5 {
         list_copy(source.head_ptr, head_ptr, tail_ptr);
     }
 
+    // Continue the copy construcurer and create a deconstrutor
 
+    sequence::~sequence() {
+        // deconstrtor
+    }
 
     void sequence::start() {
         cursor = head_ptr;
-        precursor = nullptr;
+        precursor = NULL;
     }
 
     void sequence::advance() {
@@ -30,14 +34,33 @@ namespace main_savich_5 {
     }
 
     void sequence::insert(const value_type& entry) {
-        if (cursor != nullptr || cursor = head_ptr) {
+        if (cursor != NULL || cursor == head_ptr) {
             list_head_insert(head_ptr, entry);
             cursor = head_ptr;
         }
+        ++usedl;
     }
 
     void sequence::attach(const value_type& entry) {
-        
+        if (cursor == NULL) {
+            list_head_insert(head_ptr, entry)
+            cursor = head_ptr;
+            tail_ptr = head_ptr;
+        } else {
+            list_insert(tail_ptr, entry);
+            precursor = tail_ptr;
+            tail_ptr = tail_ptr->link();
+        }
+    }
+
+    void sequence::remove_current() {
+        list_remove(precursor);
+        cursor = precursor->link();
+        --used;
+    }
+
+    const bool sequence::is_item() {
+        return 
     }
 }
 
